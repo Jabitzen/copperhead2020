e71c20926a33fef363ac5eccc716547d4a179543
Jabitzen <inventedbaton7@gmail.com>
2019-07-30 16:35:35 -0500

    update to 5.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newYearTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/newYearTest.java
@@ -38,8 +38,6 @@ public class newYearTest extends LinearOpMode {
             // Apply powers to motor
             motorR.setPower(rightPower);
             motorL.setPower(leftPower);
-
-            //Cool it works
         }
     }
 }
